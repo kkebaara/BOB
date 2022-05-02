be6945e6efff34bf5c3931d8296af7c855b21f76
Kenneth Kebaara <kkebaara@yahoo.com>
2022-05-02 11:47:38 -0600

    fixed something

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,24 @@
 class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-    before_action :authorize, only: [:show]
-    
-  def index
-    users = User.all
-    render json: users, status: :ok
-  end
-
-  def create
-    user = User.create(user_params)
-    if user.valid?
-      user.create_journal(wishlist: "", itenerary: "", other: "")
-      session[:user_id] = user.id
-      render json: user, status: :created
-    else
-      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-
-  def update
-      user = User.find_by(id: session[:user_id])
-      if user
-          user.update(user_params)
-          render json: user, status: :accepted
-      else
-          render json: { error: "Journal not found" }, status: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  skip_before_action :authorized, only: [:create, :show]
+  
+      def show
+          user = User.find(session[:user_id])
+          render json: user
+      end
+  
+      def create 
+          user = User.create!(user_params)
+          render json: user
+      end
+  
+      private
+  
+      def user_params
+          params.permit(:username, :password)
+      end
+  
+      def render_unprocessable_entity(invalid)
+          render json: {error: {login: invalid.record.errors}}, status: :unprocessable_entity
       end
   end
-
-
-  def show
-    user = User.find_by(id: session[:user_id])
-    render json: user
-  end
-
-  private
-
-  def authorize
-    render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
-  end
-
-  def user_params
-    params.permit(:username, :password, :password_confirmation)
-  end
-
-end
